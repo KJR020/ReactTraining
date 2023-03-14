580238c4171c6768949c5e38571ba13f804162b7
Kojiro <johnjiro1114@gmail.com>
2023-03-14 23:18:04 +0900

    Tue, Mar 14, 2023 11:18:04 PM

--- src/App.js
+++ src/App.js
@@ -2,24 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [description, setDescription] = useState('クリック前の表示');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          書き換えてみる
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+    ゼロから始めるReact入門
+    <List title="取り扱い言語一覧" />
+   </div>
   );
 }
 
